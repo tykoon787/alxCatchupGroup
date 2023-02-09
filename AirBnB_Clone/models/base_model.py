d04134393c3da66320ddd9f8e341aabff763e0d5
tykoon787 <tykoon787@protonmail.com>
2023-02-09 23:47:27 +0300

    Updated Base Model, works fine

--- AirBnB_Clone/models/base_model.py
+++ AirBnB_Clone/models/base_model.py
@@ -46,18 +46,3 @@ class BaseModel():
             if key == "updated_at":
                 final_dict[key] = datetime.isoformat(value) 
         return(final_dict)
-
-if __name__ == "__main__":
-
-    all_objs = storage.all()
-    print("-- Reloaded objects --")
-    for obj_id in all_objs.keys():
-        obj = all_objs[obj_id]
-        print(obj)
-
-    print("-- Create a new object --")
-    my_model = BaseModel()
-    my_model.name = "My_First_Model"
-    my_model.my_number = 89
-    my_model.save()
-    print(my_model)
