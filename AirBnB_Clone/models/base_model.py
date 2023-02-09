ccf988af2b6b52929e91358c3dd08dca37efa457
tykoon787 <tykoon787@protonmail.com>
2023-02-09 22:19:24 +0300

    Updated Code, to be reverted

--- AirBnB_Clone/models/base_model.py
+++ AirBnB_Clone/models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from uuid import uuid4
 from datetime import datetime
+from models import storage
 
 class BaseModel():
     def __init__(self, *args, **kwargs): 
@@ -13,13 +14,14 @@ class BaseModel():
             for key, value in kwargs.items():
                 if key != "__class__":
                     if key == "created_at":
-                        value = value.strptime(value, date_time_format)
+                        datetime.strptime(value, date_time_format)
                     if key == "updated_at":
-                        value = value.strptime(value, date_time_format)
+                        datetime.strptime(value, date_time_format)
                     setattr(self, key, value)
        else:
            self.id = uuid4()        
            self.created_at = datetime.now()
+           storage.new(self)
 
     def __str__(self):
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
@@ -28,44 +30,35 @@ class BaseModel():
         """
         Function taht updates the public instance attribute `updated at` with the current date and time
         """
+        storage.save()
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """
         Function that returns a dict rep of an instance
         """
-        date_time_format = "%Y-%m-%dT%H:%M:%S.%f"
         final_dict = {}
         key = "__class__"
         value = self.__class__.__name__
         final_dict[key] = value
         for key, value in self.__dict__.items():
             if key == "created_at":
-                final_dict[key] = value.strftime(date_time_format)
+                final_dict[key] = datetime.isoformat(value)
             if key == "updated_at":
-                final_dict[key] = value.strftime(date_time_format)
+                final_dict[key] = datetime.isoformat(value) 
         return(final_dict)
 
 if __name__ == "__main__":
 
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-print(my_model.id)
-print(my_model)
-print(type(my_model.created_at))
-print("--")
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+    all_objs = storage.all()
+    print("-- Reloaded objects --")
+    for obj_id in all_objs.keys():
+        obj = all_objs[obj_id]
+        print(obj)
 
-print("--")
-my_new_model = BaseModel(**my_model_json)
-print(my_new_model.id)
-print(my_new_model)
-print(type(my_new_model.created_at))
-
-print("--")
-print(my_model is my_new_model)
+    print("-- Create a new object --")
+    my_model = BaseModel()
+    my_model.name = "My_First_Model"
+    my_model.my_number = 89
+    my_model.save()
+    print(my_model)
