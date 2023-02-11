df5e4c4f4447b13fe3b36c0ee32835ebc81bf0fc
tykoon787 <tykoon787@protonmail.com>
2023-02-11 22:13:31 +0300

    Added User Class

---
+++ AirBnB_Clone/models/user.py
@@ -0,0 +1,11 @@
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+    def __init__(self):
+        super().__init__()
+
+
